9ef8c776a116edaa88341f00bc010f4127e418e0
阿里云 <2296396559@qq.com>
2024-02-19 22:50:55 +0800

    包含头文件

---
+++ inc/wrap.h
@@ -0,0 +1,44 @@
+#ifndef _WRAP_
+#define _WRAP_ 
+
+
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <sys/sendfile.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <sys/select.h>
+#include <poll.h>
+#include <errno.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <bits/sigset.h>
+#include <bits/signum.h>
+#include <signal.h>
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+
+ 
+#endif  // WRAP
+
